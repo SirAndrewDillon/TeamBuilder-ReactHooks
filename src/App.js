66223e8d772f7683be42d9675e73d7d6b25585f1
Andrew Dillon <majik895@gmail.com>
2019-07-15 13:52:04 -0400

    setting state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TeamMembers from './TeamMenbers'
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleNameChange = event => {
+    setUser({ ...user, username: event.target.value });
+  };
+
+  const handlePasswordChange = event => {
+    setUser({ ...user, password: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(user.name);
+    console.log(user.password);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(user)}
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            placeholder='Enter Name'
+            onChange={event => handleNameChange(event)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onChange={event => handlePasswordChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/TeamMenbers.jsx
@@ -0,0 +1,35 @@
+export let user = [{
+    id: 1,
+    name: "Betty Boop",
+    email: "boop@abc.com",
+    phone: "555-555-5555",
+    role: "class clown"
+}, {
+    id: 2,
+    name: "Phil These",
+    email: "phil@xyz.org",
+    phone: "444-444-4444",
+    role: "Developer"
+}, {
+    id: 3,
+    name: "Jelly Janet",
+    email: "jelly@aol.com",
+    phone: "999-999-9999",
+    role: "UX Designer"
+}, {
+    id: 4,
+    name: "Pistol Pete",
+    email: "pete@msn.net",
+    phone: "777-777-7777",
+    role: "Web Designer"
+}, {
+    id: 5,
+    name: "Moberster Mike",
+    phone: "222-222-2222",
+    role: "Backend"
+}, {
+    id: 6,
+    name: "Tony Two Times",
+    email: "tony@kik.xxx",
+    role: "Project Manager"
+}]
