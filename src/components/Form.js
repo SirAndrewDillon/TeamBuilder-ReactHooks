c53cb70110ed5102723751c54e67bcdddfdcbef4
Andrew Dillon <majik895@gmail.com>
2019-07-15 19:02:50 -0400

    glowing inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Form(props) {
   const [member, setMember] = useState({
@@ -8,6 +8,16 @@ export default function Form(props) {
     role: ""
   });
   console.log(member);
+
+//   useEffect(() => {
+//     if (props.memberToEdit) {
+//       setMemberState({
+//         name: props.memberToEdit.name,
+//         email: props.memberToEdit.email,
+//         role: props.memberToEdit.role
+//       });
+//     }
+//   }, [props.memberToEdit]);
 
   const handleChange = event => {
     setMember({ ...member, [event.target.name]: event.target.value });
@@ -57,7 +67,10 @@ export default function Form(props) {
           onChange={event => handleChange(event)}
         />
       </label>
-      <button className="btn">Submit</button> <span classNane ="money"><h3>IF IT DON'T MAKE MONEY IT DON'T MAKE CENTS!</h3></span>
+      <button className="btn">Submit</button> <span classNane ="money"><h3><marquee>To see a World in a Grain of Sand
+And a Heaven in a Wild Flower,
+Hold Infinity in the palm of your hand
+And Eternity in an hour.!</marquee></h3></span>
     </form>
   );
 }
